f8b4544413eb5524c4cad94c352dbe9fa78fc45c
Ari Pollak <ari.pollak@mail.utoronto.ca>
2026-09-15 12:04:39 -0400

    Sep15

--- ars.sh
+++ ars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=192
-#SBATCH --time=12:00:00
+#SBATCH --time=6:00:00
 #SBATCH --job-name=ars
 #SBATCH --output=/scratch/aripoll/athena_out/outputs/ars.out
 #SBATCH --mail-user=arispollak@gmail.com
